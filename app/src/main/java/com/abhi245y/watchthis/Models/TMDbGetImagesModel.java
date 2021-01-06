7f71ff1c3cefa853166c1944dbe9dd70c67c69b4
Abhiram R L <abhi@biju360>
2021-01-06 16:05:36 +0530

    new improved card design with model class getter and setter bug fixed

--- app/src/main/java/com/abhi245y/watchthis/Models/TMDbGetImagesModel.java
+++ app/src/main/java/com/abhi245y/watchthis/Models/TMDbGetImagesModel.java
@@ -20,6 +20,30 @@ public class TMDbGetImagesModel {
     private List<BackdropsBean> backdrops;
     private List<PostersBean> posters;
 
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public List<BackdropsBean> getBackdrops() {
+        return backdrops;
+    }
+
+    public void setBackdrops(List<BackdropsBean> backdrops) {
+        this.backdrops = backdrops;
+    }
+
+    public List<PostersBean> getPosters() {
+        return posters;
+    }
+
+    public void setPosters(List<PostersBean> posters) {
+        this.posters = posters;
+    }
+
     @NoArgsConstructor
     @Data
     public static class BackdropsBean {
@@ -40,6 +64,63 @@ public class TMDbGetImagesModel {
         private float vote_average;
         private float vote_count;
         private float width;
+
+
+        public Double getAspect_ratio() {
+            return aspect_ratio;
+        }
+
+        public void setAspect_ratio(Double aspect_ratio) {
+            this.aspect_ratio = aspect_ratio;
+        }
+
+        public String getFile_path() {
+            return file_path;
+        }
+
+        public void setFile_path(String file_path) {
+            this.file_path = file_path;
+        }
+
+        public float getHeight() {
+            return height;
+        }
+
+        public void setHeight(float height) {
+            this.height = height;
+        }
+
+        public Object getIso_639_1() {
+            return iso_639_1;
+        }
+
+        public void setIso_639_1(Object iso_639_1) {
+            this.iso_639_1 = iso_639_1;
+        }
+
+        public float getVote_average() {
+            return vote_average;
+        }
+
+        public void setVote_average(float vote_average) {
+            this.vote_average = vote_average;
+        }
+
+        public float getVote_count() {
+            return vote_count;
+        }
+
+        public void setVote_count(float vote_count) {
+            this.vote_count = vote_count;
+        }
+
+        public float getWidth() {
+            return width;
+        }
+
+        public void setWidth(float width) {
+            this.width = width;
+        }
     }
 
     @NoArgsConstructor
@@ -62,5 +143,61 @@ public class TMDbGetImagesModel {
         private Double vote_average;
         private Integer vote_count;
         private Integer width;
+
+        public Double getAspect_ratio() {
+            return aspect_ratio;
+        }
+
+        public void setAspect_ratio(Double aspect_ratio) {
+            this.aspect_ratio = aspect_ratio;
+        }
+
+        public String getFile_path() {
+            return file_path;
+        }
+
+        public void setFile_path(String file_path) {
+            this.file_path = file_path;
+        }
+
+        public Integer getHeight() {
+            return height;
+        }
+
+        public void setHeight(Integer height) {
+            this.height = height;
+        }
+
+        public String getIso_639_1() {
+            return iso_639_1;
+        }
+
+        public void setIso_639_1(String iso_639_1) {
+            this.iso_639_1 = iso_639_1;
+        }
+
+        public Double getVote_average() {
+            return vote_average;
+        }
+
+        public void setVote_average(Double vote_average) {
+            this.vote_average = vote_average;
+        }
+
+        public Integer getVote_count() {
+            return vote_count;
+        }
+
+        public void setVote_count(Integer vote_count) {
+            this.vote_count = vote_count;
+        }
+
+        public Integer getWidth() {
+            return width;
+        }
+
+        public void setWidth(Integer width) {
+            this.width = width;
+        }
     }
 }
--- app/src/main/java/com/abhi245y/watchthis/Models/TMDbIDModel.java
+++ app/src/main/java/com/abhi245y/watchthis/Models/TMDbIDModel.java
@@ -21,5 +21,46 @@ public class TMDbIDModel {
     private Object facebook_id;
     private Object instagram_id;
     private Object twitter_id;
+
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getImdb_id() {
+        return imdb_id;
+    }
+
+    public void setImdb_id(String imdb_id) {
+        this.imdb_id = imdb_id;
+    }
+
+    public Object getFacebook_id() {
+        return facebook_id;
+    }
+
+    public void setFacebook_id(Object facebook_id) {
+        this.facebook_id = facebook_id;
+    }
+
+    public Object getInstagram_id() {
+        return instagram_id;
+    }
+
+    public void setInstagram_id(Object instagram_id) {
+        this.instagram_id = instagram_id;
+    }
+
+    public Object getTwitter_id() {
+        return twitter_id;
+    }
+
+    public void setTwitter_id(Object twitter_id) {
+        this.twitter_id = twitter_id;
+    }
 }
 
